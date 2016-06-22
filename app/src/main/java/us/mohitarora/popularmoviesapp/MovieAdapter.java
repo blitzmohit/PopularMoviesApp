e7c74c20f14a6960d6cc6bfcbade91dd3576698d
Mohit Arora <blitz.mohit@gmail.com>
2016-06-22 19:13:26 -0400

    selection highlighting

--- app/src/main/java/us/mohitarora/popularmoviesapp/MovieAdapter.java
+++ app/src/main/java/us/mohitarora/popularmoviesapp/MovieAdapter.java
@@ -1,6 +1,7 @@
 package us.mohitarora.popularmoviesapp;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -26,6 +27,12 @@ class MovieAdapter extends ArrayAdapter<MovieItem>{
 
     public static String selected;
 
+    private View topBanner;
+
+    private View bottomBanner;
+
+    private boolean mDualPane;
+
 
     public MovieAdapter(Context context, List<MovieItem> objects) {
         super(context, 0 , objects);
@@ -45,6 +52,22 @@ class MovieAdapter extends ArrayAdapter<MovieItem>{
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.movie_item, parent, false);
         }
 
+        topBanner = convertView.findViewById(R.id.top_ribbon);
+
+        bottomBanner = convertView.findViewById(R.id.bottom_ribbon);
+
+        if( !movieItem.getId().equals( selected ) ){
+            topBanner.setVisibility(View.GONE);
+
+            bottomBanner.setVisibility(View.GONE);
+        }else{
+            if( mDualPane ){
+                topBanner.setVisibility(View.VISIBLE);
+
+                bottomBanner.setVisibility(View.VISIBLE);
+            }
+        }
+
         poster = (NetworkImageView)convertView.findViewById(R.id.poster);
 
         poster.setImageUrl( movieItem.getPosterUri(), mImageLoader );
@@ -53,6 +76,14 @@ class MovieAdapter extends ArrayAdapter<MovieItem>{
         poster.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                mDualPane = context.getResources().getBoolean(R.bool.has_two_panes);
+
+                if( mDualPane ){
+                    Log.d("Adapter","it is dualpane");
+
+                    notifyDataSetChanged();
+                }
+
                 listener.onPosterSelected(movieItem);
 
                 selected = movieItem.getId();
